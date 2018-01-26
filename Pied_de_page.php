4085b4f51ddf6c57ab833196e4671bda25c087ab
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-01-26 09:56:28 +0100

    Division du code en plusieurs fichiers
    Les parties communes du site ont été sub-divisées en plusieurs fichiers :
    - Connexion
    - Menu
    - Pied de page

---
+++ Pied_de_page.php
@@ -0,0 +1,5 @@
+<P>Site developpe par Clement Delalandre</P>
+<tr>
+    <td><a href="https://github.com/Delalandre-Clement">Mes Projets Github</a></td>
+    <td> - Profil Linkedin</td>
+</tr>
--- index.php
+++ index.php
@@ -11,30 +11,11 @@
             <header>
                 <h1><img src="Icone.png" width="25"></img> Bienvenue sur mon site web consacre au Tarot <img src="Icone.png" width="25"></h1>
 
-                <!-- Zone Connexion -->
-                <div id="log_zone">
-                    <tr>
-                        <td><label>Utilisateur :</label></td>
-                        <td><div id="user"><input type="text" name="pseudo" id="pseudo" required /></div></td>
-                    </tr>
-                    <tr>
-                        <td><label>Mot de passe :</label></td>
-                        <td><div id="mdpasse"><input type="password" name="mdp" id="mdp" required /></div></td>
-                    </tr>
-                </div>
+                <!-- Insertion de la zone de connexion -->
+                <?php include("ZoneConnexion.php"); ?>
 
-                <!-- Menu -->
-                <div id="conteneur">
-                    <div class="element"><a href="http://www.fftarot.fr/assets/documents/R-RO201206.pdf">Regles du jeu (FFTarot)</a></div>
-                    <div class="element"><a href="./Inscription.html">Inscription</a></div>
-                    <div class="element">Telecharger le jeu de tarot</div>
-                        <!--
-                            <li><a href="./PageTelechargement">Windows</a></li>
-                            <li>Linux</li>
-                            <li><a href="https://www.apple.com/">Mac OS</a></li>
-                        -->
-                    <div class="element"><a href="./ChatPublic.php">Chat Online</a></div>
-                </div>
+                <!-- Insertion du Menu -->
+                <?php include("Menu.php"); ?>
 
             </header>
 
@@ -43,11 +24,8 @@
             <p>Voici mon premier site web consacre au jeu de tarot.<br/>Ici, vous pourrez apprendre les regles du tarot et telecharger le jeu de tarot que j'ai developpe.</p>
             
             <footer>
-                <P>Site developpe par Clement Delalandre</P>
-                <tr>
-                    <td><a href="https://github.com/Delalandre-Clement">Mes Projets Github</a></td>
-                    <td> - Profil Linkedin</td>
-                </tr>
+                <!-- Insertion du Pied de page -->
+                <?php include("Pied_de_page.php"); ?>
             </footer>
         </div>
     </body>
