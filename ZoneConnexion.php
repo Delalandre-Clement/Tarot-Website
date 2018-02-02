adfb8cb1cf69a0c686e5bddd91d882d2fde452cc
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-02-02 10:19:15 +0100

    Ajoute méthode POST sur la zone de connexion

--- ZoneConnexion.php
+++ ZoneConnexion.php
@@ -1,14 +1,16 @@
 <!-- Zone Connexion -->
-<div id="log_zone">
-    <tr>
-        <td><label>Utilisateur :</label></td>
-        <td><div id="user"><input type="text" name="pseudo" id="pseudo" required /></div></td>
-    </tr>
-    <tr>
-        <td><label>Mot de passe :</label></td>
-        <td><div id="mdpasse"><input type="password" name="mdp" id="mdp" required /></div></td>
-    </tr>
-    <tr>
-        <input type="submit" value="Connexion"/>
-    </tr>
-</div>
+<form action="VerificationConnexion.php" method="post">
+    <div id="log_zone">
+        <tr>
+            <td><label>Utilisateur :</label></td>
+            <td><div id="user"><input type="text" name="pseudo" id="pseudo" required /></div></td>
+        </tr>
+        <tr>
+            <td><label>Mot de passe :</label></td>
+            <td><div id="mdpasse"><input type="password" name="mdp" id="mdp" required /></div></td>
+        </tr>
+        <tr>
+            <input type="submit" value="Connexion"/>
+        </tr>
+    </div>
+</form>
