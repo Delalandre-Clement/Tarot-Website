77c8763c4803c23e7b66667947c19cb8030160d6
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-02-03 12:36:55 +0100

    Organise page d'inscription
    Ajout menu principal et menu de login

--- PHP/Inscription.php
+++ PHP/Inscription.php
@@ -9,61 +9,70 @@ session_start();    //Demarrage de la session
     </head>
 
     <body>
-        <h1>Formulaire d'inscription</h1>
+        
+         <!-- Tête de la page -->
+        <header>
+            <h1>Formulaire d'inscription</h1>
+            <?php include("ZoneConnexion.php"); ?>
+            <?php include("Menu.php"); ?>
+        </header>
 
-        <form action="CreateAccount.php" method="post">
-            <table>
-                <tr>
-                    <td>
-                        <label>Pseudo *</label> :
-                    </td>
-                    <td>
-                        <input type="text" name="pseudo" id="pseudo" required />
-                    </td>
-                </tr>
+        <!-- Centre de la page -->
+        <section>
+            <form action="CreateAccount.php" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label>Pseudo *</label> :
+                        </td>
+                        <td>
+                            <input type="text" name="pseudo" id="pseudo" required />
+                        </td>
+                    </tr>
 
-                <tr>
-                     <td>
-                        <label>Genre :</label>
-                    </td>
-                    <td>
-                        <input type="text" name="genre" id="genre" />
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <label>Genre :</label>
+                        </td>
+                        <td>
+                            <input type="text" name="genre" id="genre" />
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td>
-                        <label>Mail *</label> :
-                    </td>
-                    <td>
-                        <input type="email" name="mail" id='mail' maxlength="25" required />
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <label>Mail *</label> :
+                        </td>
+                        <td>
+                            <input type="email" name="mail" id='mail' maxlength="25" required />
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td>
-                        <label>Mot de Passe *</label> :
-                    </td>
-                    <td>
-                        <input type="password" name="mdp" id="mdp" maxlength="50" required/>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>
-                        <label>Localisation</label> :
-                    </td>
-                    <td>
-                        <input type="text" name="lieu" id="lieu" maxlength="50"/>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <label>Mot de Passe *</label> :
+                        </td>
+                        <td>
+                            <input type="password" name="mdp" id="mdp" maxlength="50" required/>
+                        </td>
+                    </tr>
                     
-            </table>
-            <br>
-            <input type="submit" value="S'inscrire" />
-            <input type="reset" value="Reinitialiser"/>
-            <input type="button" value="Annuler" onClick="window.history.back()">
-        </form>
+                    <tr>
+                        <td>
+                            <label>Localisation</label> :
+                        </td>
+                        <td>
+                            <input type="text" name="lieu" id="lieu" maxlength="50"/>
+                        </td>
+                    </tr>
+                    <br>
+                    <input type="submit" value="S'inscrire" />
+                    <input type="reset" value="Reinitialiser"/>
+                    <input type="button" value="Annuler" onClick="window.history.back()">
+                </table>
+            </form>
+        </section>
+
         <footer>
             <!-- Insertion du Pied de page -->
             <?php include("Pied_de_page.php"); ?>
