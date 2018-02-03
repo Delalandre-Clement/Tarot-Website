6b333f33887c522e041c78a60cb7f0b548468102
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-02-03 09:32:54 +0100

    Dynamise le menu si utilisateur connecté / déconnexté
    Si connecté : propose "Chat Online"
    Si non connecté propose "Inscription"

--- Menu.php
+++ Menu.php
@@ -1,7 +1,13 @@
 <!-- Menu -->
 <div id="conteneur">
     <div class="element"><a href="http://www.fftarot.fr/assets/documents/R-RO201206.pdf">Regles du jeu (FFTarot)</a></div>
-    <div class="element"><a href="./Inscription.php">Inscription</a></div>
+    <?php 
+        if(!isset($_SESSION['pseudo'])){    
+            echo '<div class=' . '"element"' . '><a href=' . "./Inscription.php" . '>' . "Inscription" . '</a></div>';
+        }
+        else{
+            echo '<div class=' . '"element"' . '><a href=' . "./ChatPublic.php" . '>' . "Chat Online" . '</a></div>';
+        }
+    ?>
     <div class="element">Telecharger le jeu de tarot</div>
-    <div class="element"><a href="./ChatPublic.php">Chat Online</a></div>
 </div>
