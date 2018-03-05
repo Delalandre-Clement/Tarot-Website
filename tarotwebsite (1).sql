d606627025f41aaa7443f1974bb8b218ddb81b87
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-03-05 17:53:06 +0100

    Ajout du fichier de la BDD

---
+++ tarotwebsite (1).sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 05 mars 2018 à 16:51
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tarotwebsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chatpublic`
+--
+
+DROP TABLE IF EXISTS `chatpublic`;
+CREATE TABLE IF NOT EXISTS `chatpublic` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifiant Message',
+  `Pseudo` varchar(25) NOT NULL COMMENT 'Pseudo posteur',
+  `Message` text NOT NULL COMMENT 'Message',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `chatpublic`
+--
+
+INSERT INTO `chatpublic` (`ID`, `Pseudo`, `Message`) VALUES
+(5, 'Youri', 'Coucou'),
+(9, 'Yanis', 'Salut'),
+(10, 'Clement', 'Test nouveau mode de publication'),
+(11, 'Clement', 'Valide'),
+(12, 'Clement', 'Test d\'affichage'),
+(25, 'Alexi', 'Test d\'affichage'),
+(26, 'Alexi', 'Test d\'affichage'),
+(16, 'Clement', 'Hallo'),
+(18, 'Clement', 'Long'),
+(20, 'Clement', 'Je vais mettre un message qui es très long pour pouvoir tester le fonctionnement de mon chat et savoir si le texte qui est très long (vraiment très long) va dépasser de l\'écran ou alors si il va penser tout seul à sauter la ligne, je pense avoir écrit suffisament de mot pour que le texte soit suffisament long mais je préfère continuer légèrement pour en être certain, je pense que vous comprenez !'),
+(21, 'Clement', 'Un petit message de plus !'),
+(24, 'Alexi', 'tr');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identifiant Utilisateur',
+  `Pseudo` varchar(20) NOT NULL COMMENT 'Pseudo utilisateur',
+  `Genre` varchar(10) NOT NULL COMMENT 'Sexe de l''utilisateur',
+  `Mail` varchar(50) NOT NULL COMMENT 'E-mail Utilisateur',
+  `Password` varchar(50) NOT NULL COMMENT 'Mot de passe',
+  `Region` varchar(10) NOT NULL COMMENT 'Region',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`ID`, `Pseudo`, `Genre`, `Mail`, `Password`, `Region`) VALUES
+(1, 'Clement', 'Homme', 'clementdelalandre@yahoo.f', 'Tarot76', 'Le Havre'),
+(4, 'Alexi', 'Homme', 'alex@tarot.fr', 'Tarot76', 'Nice'),
+(5, 'Florian', 'Homme', 'Flo@Tarot.com', 'Tarot76', 'Marseille'),
+(6, 'Yanis', 'Homme', 'Yaya@Tarot.com', 'Tarot76', 'Frejus');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
